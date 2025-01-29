cc340ed68266504734995edf5e30cfe51493753d
sanjusahu2002 <132829320+sanjusahu2002@users.noreply.github.com>
2025-01-29 09:26:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-let table = document.getElementById("sampleTable");
-let row = document.createElement("tr");
-let cell1 = document.createElement("td");
-let cell2 = document.createElement("td");
-cell1.textContent = "New Cell1";
-cell2.textContent ="New Cell2";
-row.appendChild(cell1);
-row.appendChild(cell2);
+
 // table.insertAdjacentElement("afterbegin",row);
 function insert_Row() {
             //Write your code here
+		let table = document.getElementById("sampleTable");
 		let row = document.createElement("tr");
 		let cell1 = document.createElement("td");
 		let cell2 = document.createElement("td");
